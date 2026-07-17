b73bb69a239c0a6bd306c08bcf3c2f4f2bd95d28
Guru Akaash <sguruakash@gmail.com>
2026-07-17 13:37:43 +0530

    updating test file

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from app import app
 def test_home():
     client = app.test_client()
 
-    response = client.get("/")
+    response = client.get("/home")
 
     assert response.status_code == 200
     assert b"Hello from Flask CI/CD Demo!" in response.data
